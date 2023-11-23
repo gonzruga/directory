e8a70c331e186d4f38bc221285f10d3e12af146f
addy360 <abbyboy360@gmail.com>
2023-11-23 09:52:41 +0300

    cdn endpoint

--- src/main/java/com/reviews/Directory/utils/CdnUtils.java
+++ src/main/java/com/reviews/Directory/utils/CdnUtils.java
@@ -57,8 +57,8 @@ public class CdnUtils {
                     .addHeader("account", "directory")
 //                    .addHeader("account", "fasthub_cms")
                     .post(body)
-//                    .url("http://cdn.fasthub.co.tz/api/user/files")
-                    .url("http://192.168.66.12:9696/api/user/files")
+                    .url("http://cdn.fasthub.co.tz/api/user/files")
+//                    .url("http://192.168.66.12:9696/api/user/files")
                     .build();
 
             log.info("Started uploading image : {}", multipartFile.getOriginalFilename());
