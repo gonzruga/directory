37c9ab9e58a77c6028b2a3f57eab06ac65756847
gonzruga <grugambwa@gmail.com>
2023-12-01 16:19:35 +0200

    Deactivate AWS

--- src/main/java/com/reviews/Directory/controller/StorageController.java
+++ src/main/java/com/reviews/Directory/controller/StorageController.java
@@ -1,43 +1,43 @@
-package com.reviews.Directory.controller;
-
-import com.reviews.Directory.service.StorageService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-@RestController // Use with Postman
-@RequestMapping("/file")
-public class StorageController {
-
-    @Autowired
-    private StorageService service;
-
-    @PostMapping("/upload")
-    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
-        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
-    }
-
-//    Not implemented in UI
-    @GetMapping("/download/{fileName}")
-    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
-        byte[] data = service.downloadFile(fileName);
-        ByteArrayResource resource = new ByteArrayResource(data);
-        return ResponseEntity
-                .ok()
-                .contentLength(data.length)
-                .header("Content-type", "application/octet-stream")
-                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
-                .body(resource);
-    }
-
-//    Not implemented in UI
-    @DeleteMapping("/delete/{fileName}")
-    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
-        return new ResponseEntity<>(service.deleteFile(fileName), HttpStatus.OK);
-    }
-
-
-}
+//package com.reviews.Directory.controller;
+//
+//import com.reviews.Directory.service.StorageService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.core.io.ByteArrayResource;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//@RestController // Use with Postman
+//@RequestMapping("/file")
+//public class StorageController {
+//
+//    @Autowired
+//    private StorageService service;
+//
+//    @PostMapping("/upload")
+//    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+//        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+//    }
+//
+////    Not implemented in UI
+//    @GetMapping("/download/{fileName}")
+//    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+//        byte[] data = service.downloadFile(fileName);
+//        ByteArrayResource resource = new ByteArrayResource(data);
+//        return ResponseEntity
+//                .ok()
+//                .contentLength(data.length)
+//                .header("Content-type", "application/octet-stream")
+//                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+//                .body(resource);
+//    }
+//
+////    Not implemented in UI
+//    @DeleteMapping("/delete/{fileName}")
+//    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+//        return new ResponseEntity<>(service.deleteFile(fileName), HttpStatus.OK);
+//    }
+//
+//
+//}
