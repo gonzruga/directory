86d6523aa012367cdf36f505c374c864bb65afb7
gonzruga <grugambwa@gmail.com>
2023-12-05 15:59:13 +0200

    Deactivate AWS classes

--- src/main/java/com/reviews/Directory/Config/AwsStorageConfig.java
+++ src/main/java/com/reviews/Directory/Config/AwsStorageConfig.java
@@ -1,39 +1,39 @@
-package com.reviews.Directory.Config;
-
-
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-
-
-@Configuration
-public class AwsStorageConfig {
-
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String accessSecret;
-
-    @Value("${cloud.aws.region.static}")
-    private String region;
-
-    //TODO: remove 'Primary' Bean to recheck error of S3Client having two Beans.
-
-    @Primary
-    @Bean
-    public AmazonS3 createS3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, accessSecret);
-        return AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(region).build();
-    }
-
-}
+//package com.reviews.Directory.Config;
+//
+//
+////import com.amazonaws.auth.AWSCredentials;
+////import com.amazonaws.auth.AWSStaticCredentialsProvider;
+////import com.amazonaws.auth.BasicAWSCredentials;
+////import com.amazonaws.services.s3.AmazonS3;
+////import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//
+//
+//
+//@Configuration
+//public class AwsStorageConfig {
+//
+//    @Value("${cloud.aws.credentials.access-key}")
+//    private String accessKey;
+//
+//    @Value("${cloud.aws.credentials.secret-key}")
+//    private String accessSecret;
+//
+//    @Value("${cloud.aws.region.static}")
+//    private String region;
+//
+//    //TODO: remove 'Primary' Bean to recheck error of S3Client having two Beans.
+//
+//    @Primary
+//    @Bean
+//    public AmazonS3 createS3Client() {
+//        AWSCredentials credentials = new BasicAWSCredentials(accessKey, accessSecret);
+//        return AmazonS3ClientBuilder.standard()
+//                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+//                .withRegion(region).build();
+//    }
+//
+//}
